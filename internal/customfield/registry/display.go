345ec0d940b688d4cce5fbd852aaee3cdeacb184
faciam-dev <masami_murai@faciam.net>
2025-06-30 19:49:06 +0900

    Make display options optional

--- internal/customfield/registry/display.go
+++ internal/customfield/registry/display.go
@@ -2,14 +2,14 @@ package registry
 
 // DisplayOption represents select options for display widgets.
 type DisplayOption struct {
-	Value    string `yaml:"value"`
-	LabelKey string `yaml:"labelKey"`
+	Value    string `json:"value" yaml:"value"`
+	LabelKey string `json:"labelKey" yaml:"labelKey"`
 }
 
 // DisplayMeta holds UI related metadata for a field.
 type DisplayMeta struct {
-	LabelKey       string          `yaml:"labelKey"`
-	Widget         string          `yaml:"widget"`
-	PlaceholderKey string          `yaml:"placeholderKey"`
-	Options        []DisplayOption `yaml:"options,omitempty"`
+	LabelKey       string          `json:"labelKey" yaml:"labelKey"`
+	Widget         string          `json:"widget" yaml:"widget"`
+	PlaceholderKey string          `json:"placeholderKey" yaml:"placeholderKey"`
+	Options        []DisplayOption `json:"options,omitempty" yaml:"options,omitempty"`
 }
