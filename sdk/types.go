f12ee5e8548f60d0016a2130e5dd9a5c6ab0aa41
Masami Murai <masami_murai@faciam.net>
2025-07-02 14:12:12 +0900

    feat: add DisplayMeta and DisplayOptions types to sdk package

--- sdk/types.go
+++ sdk/types.go
@@ -3,3 +3,7 @@ package sdk
 import "github.com/faciam-dev/gcfm/internal/customfield/registry"
 
 type FieldMeta = registry.FieldMeta
+
+type DisplayMeta = registry.DisplayMeta
+
+type DisplayOptions = registry.DisplayOption
