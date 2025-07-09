fff972f4edaf472fea063baac16eed71ad3cd113
faciam-dev <masami_murai@faciam.net>
2025-07-09 17:43:01 +0900

    Fix multitenant migration for MySQL

--- internal/customfield/migrator/sql/0010_multitenant.up.sql
+++ internal/customfield/migrator/sql/0010_multitenant.up.sql
@@ -1,10 +1,12 @@
 ALTER TABLE gcfm_custom_fields
-  ADD COLUMN tenant_id VARCHAR(64) NOT NULL DEFAULT 'default',
+  ADD COLUMN tenant_id VARCHAR(64) NOT NULL DEFAULT 'default';
+ALTER TABLE gcfm_custom_fields
   DROP PRIMARY KEY,
   ADD PRIMARY KEY (tenant_id, table_name, column_name);
 
 ALTER TABLE gcfm_audit_logs
-  ADD COLUMN tenant_id VARCHAR(64) NOT NULL DEFAULT 'default',
+  ADD COLUMN tenant_id VARCHAR(64) NOT NULL DEFAULT 'default';
+ALTER TABLE gcfm_audit_logs
   DROP PRIMARY KEY,
   ADD PRIMARY KEY (tenant_id, id);
 
