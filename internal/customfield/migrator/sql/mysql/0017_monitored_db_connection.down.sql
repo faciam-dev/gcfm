a00dce8a9abc72bed256945a736efd51c0e6a071
faciam-dev <masami_murai@faciam.net>
2025-08-09 23:24:26 +0900

    fix mysql migration syntax

--- internal/customfield/migrator/sql/mysql/0017_monitored_db_connection.down.sql
+++ internal/customfield/migrator/sql/mysql/0017_monitored_db_connection.down.sql
@@ -1,6 +1,6 @@
 ALTER TABLE monitored_databases
-  DROP COLUMN IF EXISTS schema_name,
-  DROP COLUMN IF EXISTS dsn,
-  DROP COLUMN IF EXISTS driver;
+  DROP COLUMN schema_name,
+  DROP COLUMN dsn,
+  DROP COLUMN driver;
 
 DELETE FROM gcfm_registry_schema_version WHERE version=17;
